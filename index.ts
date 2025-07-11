3cd7543ba1bcd64e25e8238c4b3c4cd725e67f47
James Warren <ted3@protonmail.com>
2025-07-11 08:46:20 +0000

    sharp

--- index.ts
+++ index.ts
@@ -69,7 +69,7 @@ serve({
           pipeline = pipeline.avif({
             quality: 75,
             lossless: false,
-            effort: 3,
+            effort: 6,
             chromaSubsampling: "4:2:0"
           });
         }
